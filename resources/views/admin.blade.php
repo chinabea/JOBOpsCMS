730e9220224593619c75b23728a3b01fe2a73132
China T. Bea <chibea@my.cspc.edu.ph>
2024-04-20 05:48:55 +0800

    for faqs added

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -7,26 +7,12 @@
 </head>
 <body>
     <h1>Admin Successfully login!!</h1>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    <a href="users">users</a> <br>
+    <a href="{{ route('create.ticket') }}">Create ticket</a> <br>
+    <a href="tickets">ticket</a> <br>
+    <a href="{{ route('faqs') }}">FAQs</a> <br>
+    <a href="{{ route('create.faq') }}">Create FAQs</a> <br>
 
 
 </body>
