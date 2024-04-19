949e7e8d5ce475863f36e319b35265a357b0fe27
China T. Bea <chibea@my.cspc.edu.ph>
2024-04-20 05:49:08 +0800

    added seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Ticket;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +17,79 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        
+        $users = [
+            [
+                'user_id' => '1',
+                'first_name' => 'China',
+                'last_name' => 'Bea',
+                'email' => 'chibea@my.cspc.edu.ph',
+                'role' => 1,
+            ],
+            [
+                'user_id' => '2',
+                'first_name' => 'Jessica',
+                'last_name' => 'Mataya',
+                'email' => 'jesmataya@my.cspc.edu.ph',
+                'role' => 2,
+            ],
+            [
+                'user_id' => '3',
+                'first_name' => 'John Carlo',
+                'last_name' => 'Dacara',
+                'email' => 'johdacara@my.cspc.edu.ph',
+                'role' => 2,
+            ],
+            [
+                'user_id' => '4',
+                'first_name' => 'Mark Louis',
+                'last_name' => 'Odavar',
+                'email' => 'mlodavar@my.cspc.edu.ph',
+                'role' => 1,
+            ],
+        ];
+
+        foreach ($users as $userData) {
+            User::create($userData);
+        }
+
+        
+        $tickets = [
+            [
+                'unit' => 'MIS',
+                'request' => 'Damage Printer',
+                'description' => 'Printer Damage Damage Damage Printer Printer',
+                'user_id' => 2,
+            ],
+            [
+                'unit' => 'MIT',
+                'request' => 'aircon',
+                'description' => 'Damage Damage Printer Printer',
+                'user_id' => 2,
+            ],
+        ];
+
+        foreach ($tickets as $ticketData) {
+            Ticket::create($ticketData);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
