e8151cefa7c95c66edb6d95b38ba3e6074aebc1e
China T. Bea <chibea@my.cspc.edu.ph>
2024-04-23 21:26:30 +0800

    added profile

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->integer('role')->nullable();
             $table->string('email')->unique();
             $table->boolean('is_approved')->default(false); 
+            $table->string('avatar_url')->nullable();
             // $table->timestamp('email_verified_at')->nullable();
             // $table->string('password');
             $table->rememberToken();
