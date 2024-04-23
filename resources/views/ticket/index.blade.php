1587f4be0a2522f3866ea2a80a1bf25a4afb93bd
China T. Bea <chibea@my.cspc.edu.ph>
2024-04-23 21:25:47 +0800

    custom template

--- resources/views/ticket/index.blade.php
+++ resources/views/ticket/index.blade.php
@@ -1,126 +1,99 @@
-
-
 @extends('layouts.template')
 
 @section('content')
+<div class="right_col" role="main" style="min-height: 606.8px;">
+          <div class="">
+            <div class="page-title">
+              <div class="title_left">
+                <h3>Plain Page</h3>
+              </div>
 
-<div class="content-wrapper">
-    <section class="content-header">
-    </section>
-
-    <section class="content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
-                    
-                        <div class="card">
-                            <div class="card-header border-0">
-                                <h3 class="card-title"><b>Tickets</b></h3>
-                                <div class="card-tools">
-                                <a href="{{ route('create.ticket') }}" class="btn bg-navy color-palette btn-sm float-right">
-                                    <i class="fas fa-plus"></i> Add Ticket
-                                </a>
-                                </div>
-                                    <br><br>
-                                    <form action="{{ route('generate.tickets.report') }}" method="post">
-                                        @csrf
-                                        <div class="row align-items-center">
-                                                <div class="form-group row">
-                                                    <div class="col-md-6">
-                                                        <label for="start_date">Start Date:</label>
-                                                        <input type="date" class="form-control" name="start_date" id="start_date">
-                                                    </div>
-                                                    <div class="col-md-6">
-                                                        <label for="end_date">To:</label>
-                                                        <input type="date" class="form-control" name="end_date" id="end_date">
-                                                    </div>
-                                                </div>
-                                            <div class="col-lg-4">
-                                                <div class="form-group">
-                                                    <label>Actions</label>
-                                                    <div>
-                                                        <button type="button" id="reset" class="btn btn-warning"><i class="fa fa-sync"></i> </button>
-                                                        <button type="submit" class="btn btn-info"><i class="fa fa-file-pdf"></i> Generate PDF</button>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </form>
-                            </div>
-                            <div class="card-body">
-                            <table id="example1" class="table table-bordered table-hover text-center table-striped table-sm">
-                                <thead>
-                                    <tr>
-                                        <th>#</th>
-                                        <th>Request by</th>
-                                        <th>Location</th>
-                                        <th>Unit</th>
-                                        <th>Request</th>
-                                        <th>Assigned to</th>
-                                        <th>Priotrity</th>
-                                        <th>Status</th>
-                                        <th>Action(s)</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @if($tickets->count() > 0)
-                                    @foreach($tickets as $ticket)
-                                    <tr>
-                                        <td class="align-middle">{{ $loop->iteration }}</td>
-                                        <td class="align-middle">{{ $ticket->user->name }}</td>
-                                        <td class="align-middle">{{ $ticket->service_location }}</td>
-                                        <td class="align-middle">{{ $ticket->unit }}</td>
-                                        <td class="align-middle">{{ $ticket->request }}</td>
-                                        <td class="align-middle">{{ $ticket->assignedUser->name }}</td>
-                                        <td class="align-middle">{{ $ticket->priority_level }}</td>
-                                        <td class="align-middle"><small class="badge badge-warning"><i class="far fa-clock"></i> {{ $ticket->status }}</small></td>
-                                        
-                                        <td class="align-middle">
-                                            <a href="{{ route('update.ticket', $ticket->id) }}" type="button"
-                                                class="btn btn-sm btn-warning">
-                                                <i class="fas fa-edit"></i>
-                                            </a>
-                                            <button class="btn btn-sm btn-danger" onclick="confirmDelete('{{ route('destroy.ticket', $ticket->id) }}')">
-                                                <i class="fas fa-trash"></i>
-                                            </button>
-                                        </td>
-                                    </tr>
-                                    @endforeach
-                                    @endif
-                                </tbody>
-                            </table>
-                            </div>
-                        </div>
-                    </div>
+              <div class="title_right">
+                <div class="col-md-5 col-sm-5   form-group pull-right top_search">
+                  <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search for...">
+                    <span class="input-group-btn">
+                      <button class="btn btn-default" type="button">Go!</button>
+                    </span>
+                  </div>
                 </div>
+              </div>
             </div>
+
+            <div class="clearfix"></div>
+
+            <div class="row">
+              <div class="col-md-12 col-sm-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>All Tickets</h2>
+                    <ul class="nav navbar-right panel_toolbox">
+                    <!-- <button type="button" class="">Success</button> -->
+                                <a href="{{ route('create.ticket') }}" class="btn btn-round btn-success">
+                                    <i class="fa fa-plus-square"></i> Add Ticket
+                                </a>
+                      <!-- <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                      </li>
+                      <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="#">Settings 1</a>
+                            <a class="dropdown-item" href="#">Settings 2</a>
+                          </div>
+                      </li>
+                      <li><a class="close-link"><i class="fa fa-close"></i></a>
+                      </li> -->
+                    </ul>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+                    <table id="datatable" class="table table-bordered table-hover text-center table-striped table-sm">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Request by</th>
+                                <th>Location</th>
+                                <th>Unit</th>
+                                <th>Request</th>
+                                <th>Assigned to</th>
+                                <th>Priotrity</th>
+                                <th>Status</th>
+                                <th>Action(s)</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @if($tickets->count() > 0)
+                            @foreach($tickets as $ticket)
+                            <tr>
+                                <td class="align-middle">{{ $loop->iteration }}</td>
+                                <td class="align-middle">{{ $ticket->user->name }}</td>
+                                <td class="align-middle">{{ $ticket->service_location }}</td>
+                                <td class="align-middle">{{ $ticket->unit }}</td>
+                                <td class="align-middle">{{ $ticket->request }}</td>
+                                <td class="align-middle">{{ $ticket->assignedUser->name }}</td>
+                                <td class="align-middle">{{ $ticket->priority_level }}</td>
+                                <td class="align-middle"><small class="badge badge-warning"><i class="far fa-clock"></i> {{ $ticket->status }}</small></td>
+                                
+                                <td class="align-middle">
+                                    <a href="{{ route('update.ticket', $ticket->id) }}" type="button"
+                                        class="btn btn-sm btn-warning">
+                                        <i class="fa fa-edit"></i>
+                                    </a>
+                                    <button class="btn btn-sm btn-danger" onclick="confirmDelete('{{ route('destroy.ticket', $ticket->id) }}')">
+                                        <i class="fa fa-trash"></i>
+                                    </button>
+                                </td>
+                            </tr>
+                            @endforeach
+                            @endif
+                        </tbody>
+                    </table>
+                        
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    </section>
-</div>
-
-
-<script>
-$(document).ready(function() {
-    $('#example1').DataTable();
-});
-</script>
-
-@if(session('success'))
-<script>
-    toastr.success('{{ session('success') }}');
-</script>
-@elseif(session('delete'))
-<script>
-    toastr.delete('{{ session('delete') }}');
-</script>
-@elseif(session('message'))
-<script>
-    toastr.message('{{ session('message') }}');
-</script>
-@elseif(session('error'))
-<script>
-    toastr.error('{{ session('error') }}');
-</script>
-@endif
 
 @endsection
