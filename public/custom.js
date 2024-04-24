76cd66612fb6a747577745f03b00b43a5e53073c
China T. Bea <chibea@my.cspc.edu.ph>
2024-04-24 19:33:27 +0800

    created custom js file

---
+++ public/custom.js
@@ -0,0 +1,10 @@
+// reload page in generate report 
+$(document).ready(function() {
+
+        $('#reset').on('click', function(){
+            // Reload the page on reset button click
+            location.reload();
+
+        });
+
+});
