929a8dbe19c3c9529d7c73524954627dd7f635f1
China T. Bea <chibea@my.cspc.edu.ph>
2024-04-24 19:33:01 +0800

    added seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use App\Models\Ticket;
+use Carbon\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
@@ -36,11 +37,12 @@ class DatabaseSeeder extends Seeder
                 'role' => 2,
                 'is_approved' => true,
             ],
-            // [
-            //     'user_id' => '3',
-            //     'email' => 'johdacara@my.cspc.edu.ph',
-            //     'role' => 2,
-            // ],
+            [
+                'name' => 'John Carlo Dacara',
+                'email' => 'johdacara@my.cspc.edu.ph',
+                'role' => 2,
+                'is_approved' => true,
+            ],
             // [
             //     'user_id' => '4',
             //     'email' => 'mlodavar@my.cspc.edu.ph',
@@ -53,24 +55,91 @@ class DatabaseSeeder extends Seeder
         }
 
         
-        // $tickets = [
-        //     [
-        //         'unit' => 'MIS',
-        //         'request' => 'Damage Printer',
-        //         'description' => 'Printer Damage Damage Damage Printer Printer',
-        //         'user_id' => 2,
-        //     ],
-        //     [
-        //         'unit' => 'MIT',
-        //         'request' => 'aircon',
-        //         'description' => 'Damage Damage Printer Printer',
-        //         'user_id' => 2,
-        //     ],
-        // ];
+        $tickets = [
+            
+            [
+                'user_id' => 3,
+                'service_location' => 'Academic Building V',
+                'unit' => 'MIS',
+                'request' => 'Damage Printer',
+                'description' => 'Printer Damage Damage Damage Printer Printer',
+                'priority_level' => 'High',
+                'deadline' => Carbon::create(2023, 12, 22),
+                'status' => 'Open',
+                'assigned_to' => 2
+            ],
+            [
+                'user_id' => 3,
+                'service_location' => 'Cashier Office',
+                'unit' => 'MIT',
+                'request' => 'aircon',
+                'description' => 'Damage Damage Printer Printer',
+                'priority_level' => 'Mid',
+                'deadline' => Carbon::create(2023, 12, 22),
+                'status' => 'In Progress',
+                'assigned_to' => 2
+            ],
+            [
+                'user_id' => 3,
+                'service_location' => 'Office',
+                'unit' => 'Repair',
+                'request' => 'ID issues',
+                'description' => 'Need for Repair',
+                'priority_level' => 'High',
+                'deadline' => Carbon::create(2023, 02, 22),
+                'status' => 'Closed',
+                'assigned_to' => 2
+            ],
+            [
+                'user_id' => 3,
+                'service_location' => 'Cashier',
+                'unit' => 'Repair',
+                'request' => 'Repair',
+                'description' => 'Damage',
+                'priority_level' => 'Mid',
+                'deadline' => Carbon::create(2023, 10, 27),
+                'status' => 'In Progress',
+                'assigned_to' => 2
+            ],
+            
+            [
+                'user_id' => 3,
+                'service_location' => 'Academic Building V',
+                'unit' => 'MIS',
+                'request' => 'Damage Printer',
+                'description' => 'Printer Damage Damage Damage Printer Printer',
+                'priority_level' => 'High',
+                'deadline' => Carbon::create(2023, 12, 22),
+                'status' => 'Open',
+                'assigned_to' => 2
+            ],
+            [
+                'user_id' => 3,
+                'service_location' => 'Cashier Office',
+                'unit' => 'MIT',
+                'request' => 'aircon',
+                'description' => 'Damage Damage Printer Printer',
+                'priority_level' => 'Mid',
+                'deadline' => Carbon::create(2023, 12, 22),
+                'status' => 'In Progress',
+                'assigned_to' => 2
+            ],
+            [
+                'user_id' => 3,
+                'service_location' => 'Office',
+                'unit' => 'Repair',
+                'request' => 'ID issues',
+                'description' => 'Need for Repair',
+                'priority_level' => 'High',
+                'deadline' => Carbon::create(2023, 02, 22),
+                'status' => 'Closed',
+                'assigned_to' => 2
+            ],
+        ];
 
-        // foreach ($tickets as $ticketData) {
-        //     Ticket::create($ticketData);
-        // }
+        foreach ($tickets as $ticketData) {
+            Ticket::create($ticketData);
+        }
 
 
 
