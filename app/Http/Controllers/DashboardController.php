75d234e14a167172f3d227d31c13a6f1fcd5ec53
China T. Bea <chibea@my.cspc.edu.ph>
2024-04-27 12:46:07 +0800

    added tickets analytics

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,9 +2,66 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ticket;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    //
+    public function index()
+    {
+        $totalUsers = User::count();
+        $totalTickets = Ticket::count();
+        $allOpen = Ticket::where('status', 'Open')->count();
+        $allInProgress = Ticket::where('status', 'In Progress')->count();
+        $allClosed = Ticket::where('status', 'Closed')->count();
+        $allHigh = Ticket::where('priority_level', 'High')->count();
+        $allMid = Ticket::where('priority_level', 'Mid')->count();
+        $allLow = Ticket::where('priority_level', 'Low')->count();
+
+
+        // Monthly data
+        $monthlyTicketsData = Ticket::select(
+            DB::raw('MONTH(created_at) as month'),
+            DB::raw('YEAR(created_at) as year'),
+            DB::raw('COUNT(*) as count')
+        )
+        ->groupBy('year', 'month')
+        ->orderBy('year', 'asc')
+        ->orderBy('month', 'asc')
+        ->get();
+    
+        return view('dashboard', compact('monthlyTicketsData', 'totalUsers','totalTickets', 'allOpen',
+        'allInProgress', 'allClosed','allHigh', 'allMid', 'allLow'));
+    }
+    
+    // public function monthlyWeekly()
+    // {
+    //     // Monthly data
+    //     $monthlyTicketsData = Ticket::select(
+    //         DB::raw('MONTH(created_at) as month'),
+    //         DB::raw('YEAR(created_at) as year'),
+    //         DB::raw('COUNT(*) as count')
+    //     )
+    //     ->groupBy('year', 'month')
+    //     ->orderBy('year', 'asc')
+    //     ->orderBy('month', 'asc')
+    //     ->get();
+    
+    //     // Weekly data
+    //     $weeklyTicketsData = Ticket::select(
+    //         DB::raw('YEAR(created_at) as year'),
+    //         DB::raw('WEEK(created_at) as week'),
+    //         DB::raw('COUNT(*) as count')
+    //     )
+    //     ->groupBy('year', 'week')
+    //     ->orderBy('year', 'asc')
+    //     ->orderBy('week', 'asc')
+    //     ->get();
+    
+    //     return view('dashboard', compact('monthlyTicketsData', 'weeklyTicketsData'));
+    // }
+    
 }
