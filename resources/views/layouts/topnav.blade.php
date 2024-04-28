a4ea7731b1493614e36b7b97e201e462671c1e41
China T. Bea <chibea@my.cspc.edu.ph>
2024-04-28 22:57:46 +0800

    update topnav

--- resources/views/layouts/topnav.blade.php
+++ resources/views/layouts/topnav.blade.php
@@ -1,7 +1,7 @@
 
         <!-- top navigation -->
         <div class="top_nav">
-          <div class="nav_menu shadow">
+          <div class="nav_menu">
               <div class="nav toggle">
                 <a id="menu_toggle"><i class="fa fa-bars"></i></a>
               </div>
