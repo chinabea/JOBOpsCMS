dc4cf4826ef8d4f6417e9d3c27b424b713a90152
China T. Bea <chibea@my.cspc.edu.ph>
2024-04-30 13:20:46 +0800

    functional ticket creation

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -11,7 +11,7 @@ class Ticket extends Model
 
     public $primaryKey = 'id';
 
-    public $fillable = ['service_location','unit', 'request', 'priority_level', 'description', 'user_id', 'is_approved', 'deadline', 'google_id', 'avatar'];
+    public $fillable = ['service_location','unit', 'request', 'priority_level', 'description', 'user_id', 'is_approved', 'deadline'];
     
     public function user()
     {
--- resources/views/ticket/create.blade.php
+++ resources/views/ticket/create.blade.php
@@ -3,129 +3,126 @@
 @section('content')
 <!-- page content -->
 <div class="right_col" role="main">
-				<div class="">
-					<div class="page-title">
-						<div class="title_left">
-							<h3>Form Elements</h3>
-						</div>
+	<div class="">
+		<div class="page-title">
+			<div class="title_left">
+				<h3>Form Elements</h3>
+			</div>
 
-						<div class="title_right">
-							<div class="col-md-5 col-sm-5  form-group pull-right top_search">
-								<div class="input-group">
-									<input type="text" class="form-control" placeholder="Search for...">
-									<span class="input-group-btn">
-										<button class="btn btn-default" type="button">Go!</button>
-									</span>
-								</div>
-							</div>
-						</div>
+			<div class="title_right">
+				<div class="col-md-5 col-sm-5  form-group pull-right top_search">
+					<div class="input-group">
+						<input type="text" class="form-control" placeholder="Search for...">
+						<span class="input-group-btn">
+							<button class="btn btn-default" type="button">Go!</button>
+						</span>
 					</div>
-					<div class="clearfix"></div>
-					<div class="row">
-						<div class="col-md-12 col-sm-12 ">
-							<div class="x_panel">
-								<div class="x_title">
-									<h2>Form Design <small>different form elements</small></h2>
-									<ul class="nav navbar-right panel_toolbox">
-										<li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-										</li>
-										<li class="dropdown">
-											<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-											<ul class="dropdown-menu" role="menu">
-												<li><a class="dropdown-item" href="#">Settings 1</a>
-												</li>
-												<li><a class="dropdown-item" href="#">Settings 2</a>
-												</li>
-											</ul>
-										</li>
-										<li><a class="close-link"><i class="fa fa-close"></i></a>
-										</li>
-									</ul>
-									<div class="clearfix"></div>
-								</div>
-								<div class="x_content">
-									<br />
-									<form id="demo-form2" data-parsley-validate class="form-horizontal form-label-left">
-
-										<div class="item form-group">
-											<label class="col-form-label col-md-3 col-sm-3 label-align" for="first-name">Service Location <span class="required">*</span>
-											</label>
-											<div class="col-md-6 col-sm-6 ">
-												<input type="text" id="first-name" name="service_location" required="required" class="form-control ">
-											</div>
-										</div>
-										<div class="item form-group">
-											<label class="col-form-label col-md-3 col-sm-3 label-align" for="last-name">Unit <span class="required">*</span>
-											</label>
-											<div class="col-md-6 col-sm-6 ">
-												<input type="text" id="last-name" name="last-name" required="required" class="form-control">
-											</div>
-										</div>
-										<div class="item form-group">
-											<label for="middle-name" class="col-form-label col-md-3 col-sm-3 label-align">Request</label>
-											<div class="col-md-6 col-sm-6 ">
-												<input id="middle-name" class="form-control" type="text" name="middle-name">
-											</div>
-										</div>
-										<div class="item form-group">
-											<label for="middle-name" class="col-form-label col-md-3 col-sm-3 label-align">Upload File</label>
-											<div class="col-md-6 col-sm-6 ">
-												<input id="middle-name" class="form-control" type="text" name="middle-name">
-											</div>
-										</div>
-										<div class="item form-group">
-											<label for="middle-name" class="col-form-label col-md-3 col-sm-3 label-align">Description</label>
-											<div class="col-md-6 col-sm-6 ">
-												<input id="middle-name" class="form-control" type="text" name="middle-name">
-											</div>
-										</div>
-										<div class="item form-group">
-											<label class="col-form-label col-md-3 col-sm-3 label-align">Priority Level</label>
-											<div class="col-md-6 col-sm-6 ">
-												<div id="gender" class="btn-group" data-toggle="buttons">
-													<label class="btn btn-secondary" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
-														<input type="radio" name="gender" value="male" class="join-btn"> &nbsp; Male &nbsp;
-													</label>
-													<label class="btn btn-primary" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
-														<input type="radio" name="gender" value="female" class="join-btn"> Female
-													</label>
-												</div>
-											</div>
-										</div>
-										<div class="item form-group">
-											<label class="col-form-label col-md-3 col-sm-3 label-align">Deadline <span class="required">*</span>
-											</label>
-											<div class="col-md-6 col-sm-6 ">
-												<input id="birthday" class="date-picker form-control" placeholder="dd-mm-yyyy" type="text" required="required" type="text" onfocus="this.type='date'" onmouseover="this.type='date'" onclick="this.type='date'" onblur="this.type='text'" onmouseout="timeFunctionLong(this)">
-												<script>
-													function timeFunctionLong(input) {
-														setTimeout(function() {
-															input.type = 'text';
-														}, 60000);
-													}
-												</script>
-											</div>
-										</div>
-										<div class="ln_solid"></div>
-										<div class="item form-group">
-											<div class="col-md-6 col-sm-6 offset-md-3">
-												<button class="btn btn-primary" type="button">Cancel</button>
-												<button class="btn btn-primary" type="reset">Reset</button>
-												<button type="submit" class="btn btn-success">Submit</button>
-											</div>
-										</div>
-
-									</form>
-								</div>
-							</div>
-						</div>
-					</div>
-
-			
-
-
 				</div>
 			</div>
-			<!-- /page content -->
+		</div>
+		<div class="clearfix"></div>
+		<div class="row">
+			<div class="col-md-12 col-sm-12 ">
+				<div class="x_panel">
+					<div class="x_title">
+						<h2>Form Design <small>different form elements</small></h2>
+						<ul class="nav navbar-right panel_toolbox">
+							<li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+							</li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+								<ul class="dropdown-menu" role="menu">
+									<li><a class="dropdown-item" href="#">Settings 1</a>
+									</li>
+									<li><a class="dropdown-item" href="#">Settings 2</a>
+									</li>
+								</ul>
+							</li>
+							<li><a class="close-link"><i class="fa fa-close"></i></a>
+							</li>
+						</ul>
+						<div class="clearfix"></div>
+					</div>
+					<div class="x_content">
+						<br />
+							<form action="{{ route('store.ticket') }}" method="post" enctype="multipart/form-data" data-parsley-validate class="form-horizontal form-label-left">
+								@csrf
+								<input class="form-control" type="hidden" name="user_id" value="{{ auth()->id() }}">
+
+							<div class="item form-group">
+								<label class="col-form-label col-md-3 col-sm-3 label-align" for="first-name">Service Location <span class="required">*</span>
+								</label>
+								<div class="col-md-6 col-sm-6 ">
+									<input type="text" id="first-name" name="service_location" required="required" class="form-control ">
+								</div>
+							</div>
+							<div class="item form-group">
+								<label class="col-form-label col-md-3 col-sm-3 label-align" for="unit">Unit <span class="required">*</span>
+								</label>
+								<div class="col-md-6 col-sm-6 ">
+									<input type="text" id="unit" name="unit" required="required" class="form-control">
+								</div>
+							</div>
+							<div class="item form-group">
+								<label class="col-form-label col-md-3 col-sm-3 label-align" for="unit">Request <span class="required">*</span>
+								</label>
+								<div class="col-md-6 col-sm-6 ">
+									<input type="text" id="request" name="request" required="required" class="form-control">
+								</div>
+							</div>
+							<div class="item form-group">
+								<label for="file_path" class="col-form-label col-md-3 col-sm-3 label-align">Upload File</label>
+								<div class="col-md-6 col-sm-6 ">
+									<input id="file_path" class="form-control" type="file" name="file_path">
+								</div>
+							</div>
+							<div class="item form-group">
+								<label for="description" class="col-form-label col-md-3 col-sm-3 label-align">Description</label>
+								<div class="col-md-6 col-sm-6 ">
+									<input id="description" class="form-control" type="text" name="description">
+								</div>
+							</div>
+							<div class="item form-group">
+								<label class="col-form-label col-md-3 col-sm-3 label-align">Priority Level</label>
+								<div class="col-md-6 col-sm-6 ">
+									<div id="gender" class="btn-group" data-toggle="buttons">
+										<label class="btn btn-danger" data-toggle-class="btn-warning" data-toggle-passive-class="btn-default">
+											<input type="radio" name="priority_level" value="high" class="join-btn"> &nbsp; High &nbsp;
+										</label>
+										<label class="btn btn-warning" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
+											<input type="radio" name="priority_level" value="mid" class="join-btn"> Mid
+										</label>
+										<label class="btn btn-secondary" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
+											<input type="radio" name="priority_level" value="low" class="join-btn"> Low
+										</label>
+									</div>
+								</div>
+							</div>
+							
+							<div class="field item form-group">
+								<label class="col-form-label col-md-3 col-sm-3  label-align">Deadline<span class="required">*</span></label>
+								<div class="col-md-6 col-sm-6">
+									<input class="form-control" class='date' type="date" name="deadline" id="deadline" required='required'></div>
+							</div>
+							<div class="ln_solid"></div>
+							<div class="item form-group">
+								<div class="col-md-6 col-sm-6 offset-md-3">
+									<button class="btn btn-primary" id="reset" type="reset">Reset</button>
+									<button type="submit" class="btn btn-success">Submit</button>
+								</div>
+							</div>
+
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+
+
+	</div>
+</div>
+<!-- /page content -->
 
 @endsection
