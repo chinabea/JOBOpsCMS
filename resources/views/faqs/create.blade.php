375d524102860eb593c4df732d8903bd621607e6
China T. Bea <chibea@my.cspc.edu.ph>
2024-05-02 21:12:41 +0800

    updated faaqs

--- resources/views/faqs/create.blade.php
+++ resources/views/faqs/create.blade.php
@@ -1,84 +1,42 @@
-
-
-
 @extends('layouts.template')
 
 @section('content')
+<div class="right_col" role="main" style="min-height: 606.8px;">
+  <div class="">
+    <div class="page-title">
+      <div class="title_left">
+        <h3>FAQ</h3>
+      </div>
+    </div>
 
-<div class="content-wrapper">
-    <section class="content-header">
-    </section>
+    <div class="clearfix"></div>
 
-    <section class="content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
+    <div class="row">
+      <div class="col-md-12 col-sm-12">
+        <div class="x_panel">
+          <div class="x_title">
+            <h2>Add F.A.Q.</h2>
+            <div class="clearfix"></div>
+          </div>
+          <div class="x_content"> 
+            <br><br>
+            <form action="{{ route('store.faq') }}" method="post">
+                @csrf
+                <label for="question">Question</label> <br>
+                <input  class="form-control" name="question" id="question" required></input><br>
 
-                
-                        <div class="card">
-                            <div class="card-header border-0">
-                                <h3 class="card-title"><b>Add FAQs</b></h3>
-                                <div class="card-tools">
-                                    <a href="#" class="btn btn-tool btn-sm">
-                                        <i class="fas fa-download"></i>
-                                    </a>
-                                    <a href="#" class="btn btn-tool btn-sm">
-                                        <i class="fas fa-bars"></i>
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="card-body">
-                                
-                                <form action="{{ route('store.faq') }}" method="post">
-                                    @csrf
+                <label for="answer">Answer</label> <br>
+                <input  class="form-control" name="answer" id="answer" required></input><br>
 
-                                    <label for="">Question</label> <br>
-                                    <input  class="form-control" name="question" id="question" required></input><br>
-
-                                    <label for="">Answer</label> <br>
-                                    <input  class="form-control" name="answer" id="answer" required></input><br>
-
-                                    <button type="submit" class="btn btn-primary col start" name="submit_project">
-                                        <i class="fas fa-upload"></i>
-                                        <span>Submit</span>
-                                    </button>
-
-                                </form>
-
-                                
-                            </div>
-                        </div>
-                    </div>
-                        
-                </div>
-            </div>
+                <button type="submit" class="btn btn-primary col start" name="submit_project">
+                    <i class="fa fa-upload"></i>
+                    <span>Submit</span>
+                </button>
+            </form>
+          </div>
         </div>
-    </section>
+      </div>
+    </div>
+  </div>
 </div>
-
-
-<script>
-$(document).ready(function() {
-    $('#example1').DataTable();
-});
-</script>
-
-@if(session('success'))
-<script>
-    toastr.success('{{ session('success') }}');
-</script>
-@elseif(session('delete'))
-<script>
-    toastr.delete('{{ session('delete') }}');
-</script>
-@elseif(session('message'))
-<script>
-    toastr.message('{{ session('message') }}');
-</script>
-@elseif(session('error'))
-<script>
-    toastr.error('{{ session('error') }}');
-</script>
-@endif
-
 @endsection
