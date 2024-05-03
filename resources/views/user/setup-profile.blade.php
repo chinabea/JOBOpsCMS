a339f77820c38b3906c329bfc738c03418b606b8
China T. Bea <chibea@my.cspc.edu.ph>
2024-05-03 20:47:24 +0800

    added FE for user set up profile

--- resources/views/user/setup-profile.blade.php
+++ resources/views/user/setup-profile.blade.php
@@ -1,35 +1,51 @@
-<form action="{{ route('user.saveProfile') }}" method="POST">
-    @csrf
-    <div>
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" value="{{ $user->name }}" readonly>
+
+@extends('layouts.guest-template')
+
+@section('content')
+
+<section id="pricing" class="pricing">
+  <div class="container">
+    <div class="row no-gutters justify-content-center">
+
+      <div class="col-lg-8 box featured" data-aos="fade-up">
+        <h4>Setup your Profile</h4>
+        <ul>
+          <form action="{{ route('user.saveProfile') }}" method="POST">
+            @csrf
+              <div class="form-group mt-3">
+                <input class="form-control" type="text" id="name" name="name" value="{{ $user->name }}" readonly>
+            </div>
+              <div class="form-group mt-3">
+                <input class="form-control" type="email" id="email" name="email" value="{{ $user->email }}" readonly>
+            </div>
+              <div class="form-group mt-3">
+                <input placeholder="Phone Number" class="form-control" type="text" id="phone_number" name="phone_number" required>
+            </div>
+              <div class="form-group mt-3">
+                <div class="col-sm-10">
+                    <select class="form-control" name="role" required>
+                            <option value="" selected disabled>Select your Role</option>
+                            <option value="1" type="Admin" id="1" name="1">Admin</option>
+                            <option value="2" type="MICT Staff" id="2" name="2">MICT Staff</option>
+                            <option value="3" type="Staff" id="3" name="3">Staff</option>
+                    </select>
+                </div>
+            </div>
+              <div class="form-group mt-3">
+                <input placeholder="Job Position" class="form-control" type="text" id="job_position" name="job_position" required>
+            </div>
+              <div class="form-group mt-3">
+                <input  placeholder="Expertise" class="form-control" type="text" id="expertise" name="expertise" required>
+            </div><br><br>
+            <div class="text-center"><a type="submit" href="#" class="get-started-btn">Save Profile and Request</a></div>
+        </form>
+        </ul>
+      </div>
+      
     </div>
-    <div>
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" value="{{ $user->email }}" readonly>
-    </div>
-    <div>
-        <label for="phone_number">Phone Number:</label>
-        <input type="text" id="phone_number" name="phone_number" required>
-    </div>
-    <div class="form-group row">
-        <label for="inputExperience" class="col-sm-2 col-form-label">Role</label>
-        <div class="col-sm-10">
-            <select class="form-control" name="role" required>
-                    <option value="" selected disabled>Please select a role</option>
-                    <option value="1" type="Admin" id="1" name="1">Admin</option>
-                    <option value="2" type="MICT Staff" id="2" name="2">MICT Staff</option>
-                    <option value="3" type="Staff" id="3" name="3">Staff</option>
-            </select>
-        </div>
-    </div>
-    <div>
-        <label for="job_position">Job Position:</label>
-        <input type="text" id="job_position" name="job_position" required>
-    </div>
-    <div>
-        <label for="expertise">Expertise:</label>
-        <input type="text" id="expertise" name="expertise" required>
-    </div>
-    <button type="submit">Save Profile</button>
-</form>
+
+  </div>
+</section>
+<!-- End Pricing Section -->
+
+@endsection
