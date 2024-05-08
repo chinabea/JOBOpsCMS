8805cc2ccf0fdc872749f1e6b974186f1bffdf30
China T. Bea <chibea@my.cspc.edu.ph>
2024-05-08 20:26:42 +0800

    fix: updated db seeder and migration tables

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -95,51 +95,70 @@ class DatabaseSeeder extends Seeder
         foreach ($users as $userData) {
             User::create($userData);
         }
+
+        
+        // $tickets = [
+        //     [
+        //         'user_id' => 1,
+        //         'building_number' => 'Acad Building 1',
+        //         'office_name' => 'Dean',
+        //         'unit' => 'ICT Repair and Maintence',
+        //         'unit_options' => '',
+        //         'phone_number' => '09628950257',
+        //         'job_position' => 'Software Developer',
+        //         'expertise' => ['Developer', 'Software Programmer'],
+                
+        //     ],
+        // ];
+
+        // foreach ($tickets as $ticketData) {
+        //     Ticket::create($ticketData);
+        // }
         
         // Ticket seeding
-        $faker = Faker::create();
-        $approvedUsers = User::where('is_approved', true)
-                      ->whereIn('role', [1, 2, 3]) 
-                      ->pluck('id')
-                      ->toArray();
+        // $faker = Faker::create();
+        // $approvedUsers = User::where('is_approved', true)
+        //               ->whereIn('role', [1, 2, 3]) 
+        //               ->pluck('id')
+        //               ->toArray();
                       
-        $serviceLocationOptions = [
-            'Academic Building V', 'Cashier Office', 'Registrar Office', 
-        ];
-        $unitOptions = ['MICT', 'MIS', 'Repair', 'Network'];
-        $requestOptions = [
-            'Unable to Access Student Database', 'Email Setup on New Staff Computers', 
-        ];
-        $priorityLevels = ['High', 'Mid', 'Low'];
-        $statusOptions = ['Open', 'In Progress', 'Closed'];
+        // $serviceLocationOptions = [
+        //     'Academic Building V', 'Cashier Office', 'Registrar Office', 
+        // ];
+        // $unitOptions = ['MICT', 'MIS', 'Repair', 'Network'];
+        // $requestOptions = [
+        //     'Unable to Access Student Database', 'Email Setup on New Staff Computers', 
+        // ];
+        // $priorityLevels = ['High', 'Mid', 'Low'];
+        // $statusOptions = ['Open', 'In Progress', 'Closed'];
 
-        for ($i = 0; $i < 70; $i++) {            
-            $randomTimestamp = $faker->dateTimeBetween('-1 years', 'now');
-            $ticket = Ticket::create([
-                'user_id'          => $faker->randomElement($approvedUsers), 
-                'service_location' => $faker->randomElement($serviceLocationOptions),
-                'unit'             => $faker->randomElement($unitOptions),
-                'request'          => $faker->randomElement($requestOptions),
-                'priority_level'   => $faker->randomElement($priorityLevels),
-                'deadline'         => $faker->date(),
-                'description'      => $faker->sentence(),
-                'file_path'        => $faker->randomElement([$faker->imageUrl(), null]),
-                'status'           => $faker->randomElement($statusOptions),
-                'created_at' => $randomTimestamp,
-                'updated_at' => $randomTimestamp,
-                // 'created_at'       => $faker->dateTimeThisYear(),
-                // 'updated_at'       => $faker->dateTimeThisYear()
-            ]);
+        // for ($i = 0; $i < 70; $i++) {            
+        //     $randomTimestamp = $faker->dateTimeBetween('-1 years', 'now');
+        //     $ticket = Ticket::create([
+        //         'user_id'          => $faker->randomElement($approvedUsers), 
+        //         'service_location' => $faker->randomElement($serviceLocationOptions),
+        //         'unit'             => $faker->randomElement($unitOptions),
+        //         'request'          => $faker->randomElement($requestOptions),
+        //         'priority_level'   => $faker->randomElement($priorityLevels),
+        //         'deadline'         => $faker->date(),
+        //         'description'      => $faker->sentence(),
+        //         'file_path'        => $faker->randomElement([$faker->imageUrl(), null]),
+        //         'status'           => $faker->randomElement($statusOptions),
+        //         'created_at' => $randomTimestamp,
+        //         'updated_at' => $randomTimestamp,
+        //         // 'created_at'       => $faker->dateTimeThisYear(),
+        //         // 'updated_at'       => $faker->dateTimeThisYear()
+        //     ]);
 
-            // Assign approved users to tickets using the pivot table
-            // Can assign multiple users or none
-            $numberOfUsersToAssign = $faker->numberBetween(0, 3); // Number of users to assign
-            $assignedUsers = $faker->randomElements($approvedUsers, $numberOfUsersToAssign); // Correctly picking random elements
+        //     // Assign approved users to tickets using the pivot table
+        //     // Can assign multiple users or none
+        //     $numberOfUsersToAssign = $faker->numberBetween(0, 3); // Number of users to assign
+        //     $assignedUsers = $faker->randomElements($approvedUsers, $numberOfUsersToAssign); // Correctly picking random elements
 
-            foreach ($assignedUsers as $userId) {
-                $ticket->users()->attach($userId); // Assuming Ticket model has users() relation defined
-            }
+        //     foreach ($assignedUsers as $userId) {
+        //         $ticket->users()->attach($userId); // Assuming Ticket model has users() relation defined
+        //     }
 
-        }
+        // }
     }
 }
