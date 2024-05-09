3d79608a535f679fdc748e1aaa59134670c7baa1
China T. Bea <chibea@my.cspc.edu.ph>
2024-05-09 09:34:22 +0800

    fix: updated unit views

--- resources/views/unit/create.blade.php
+++ resources/views/unit/create.blade.php
@@ -3,19 +3,45 @@
 @extends('layouts.template')
 
 @section('content')
-<div class="container">
-    <h1>Create Unit Type</h1>
-    <form method="POST" action="{{ route('units.store') }}">
-        @csrf
-        <div class="form-group">
-            <label for="name">Name</label>
-            <input type="text" class="form-control" id="name" name="name" required>
+
+
+
+
+<div class="content-wrapper">
+    <section class="content-header">
+    </section>
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h3 class="card-title my-1"><i class="fa fa-book"></i> <b>Submitted Projects</b></h3> <br><br>
+                            <div class="container">
+                                <h1>Create Unit Type</h1>
+                                <form method="POST" action="{{ route('units.store') }}">
+                                    @csrf
+                                    <div class="form-group">
+                                        <label for="name">Name</label>
+                                        <input type="text" class="form-control" id="name" name="name" required>
+                                    </div>
+                                    <!-- <div class="form-group">
+                                        <label for="description">Description (optional)</label>
+                                        <textarea class="form-control" id="description" name="description"></textarea>
+                                    </div> -->
+                                    <button type="submit" class="btn btn-primary">Submit</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="form-group">
-            <label for="description">Description (optional)</label>
-            <textarea class="form-control" id="description" name="description"></textarea>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-</div>
+    </section>
+</div> 
+
+
+
+
+
 @endsection
