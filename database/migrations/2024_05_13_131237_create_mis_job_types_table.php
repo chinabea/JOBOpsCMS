f4760eb2e39ff6f97117537aa1d16e5298de35b0
China T. Bea <chibea@my.cspc.edu.ph>
2024-05-13 13:31:57 +0800

    added mis migrations

---
+++ database/migrations/2024_05_13_131237_create_mis_job_types_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mis_job_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('mis_request_type_id');
+            $table->unsignedBigInteger('asname_id'); // Modify this line
+            $table->timestamps();
+
+            $table->foreign('mis_request_type_id')->references('id')->on('mis_request_types')->onDelete('cascade');
+            $table->foreign('asname_id')->references('id')->on('mis_asnames')->onDelete('cascade'); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mis_job_types');
+    }
+};
