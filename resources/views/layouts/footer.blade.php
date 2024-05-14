2ec513a3f795a04bb3ea3b4021c92bf1171c4a3f
China T. Bea <chibea@my.cspc.edu.ph>
2024-05-14 20:53:39 +0800

    update footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -5,3 +5,12 @@
       © 2024 by ChinaBea. All rights reserved.
     </div>
   </footer> -->
+
+<!-- 
+  <footer class="main-footer">
+
+  © 2024 by China Bea. All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      
+    </div>
+  </footer> -->
