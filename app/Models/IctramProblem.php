32d3fe359ddeca5b9bc6333195892e409b5a2875
China T. Bea <chibea@my.cspc.edu.ph>
2024-05-14 16:10:21 +0800

    updated model

--- app/Models/IctramProblem.php
+++ app/Models/IctramProblem.php
@@ -8,6 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class IctramProblem extends Model
 {
     use HasFactory;
-    public $fillable = ['description', 
+    public $fillable = ['problem_description', 
                         'ictram_equipment_id'];
+    
+    public function equipment()
+    {
+        return $this->belongsTo(IctramEquipment::class);
+    }
+
 }
