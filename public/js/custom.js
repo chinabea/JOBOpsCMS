bf8d3a657b029b29360d74c939754356470df7df
China T. Bea <chibea@my.cspc.edu.ph>
2024-05-15 21:29:56 +0800

    refactor: update js and css

--- public/js/custom.js
+++ public/js/custom.js
@@ -294,3 +294,14 @@ function confirmDelete(unitId) {
       deleteForm.submit();
   }
 }
+
+$(function () {
+  // Summernote
+  $('#summernote').summernote()
+
+  // CodeMirror
+  CodeMirror.fromTextArea(document.getElementById("codeMirrorDemo"), {
+  mode: "htmlmixed",
+  theme: "monokai"
+  });
+})
