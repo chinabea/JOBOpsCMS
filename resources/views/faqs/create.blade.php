959ced945810e6c1dac79863e633ee59860c0210
China T. Bea <chibea@my.cspc.edu.ph>
2024-05-15 21:31:30 +0800

    refactor: updated create faqs blade

--- resources/views/faqs/create.blade.php
+++ resources/views/faqs/create.blade.php
@@ -1,40 +1,28 @@
 
+<div class="modal fade" id="createFaqModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" id="myModalLabel">Add FAQ</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form action="{{ route('store.faq') }}" method="post">
+                    @csrf
+                    <label for="question">Question</label><br>
+                    <input class="form-control" name="question" id="question" required><br>
 
+                    <label for="answer">Answer</label><br>
+                    <textarea id="summernote" class="summernote" name="answer" required></textarea><br>
 
-@extends('layouts.template')
-
-@section('content')
-<div class="content-wrapper">
-    <section class="content-header">
-    </section>
-
-    <section class="content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
-                    <div class="card">
-                        <div class="card-body">
-                            <h3 class="card-title my-1"><i class="fa fa-book"></i> <b>Submitted Projects</b></h3> <br><br>
-                            <form action="{{ route('store.faq') }}" method="post">
-                              @csrf
-                              <label for="question">Question</label> <br>
-                              <input  class="form-control" name="question" id="question" required></input><br>
-
-                              <label for="answer">Answer</label> <br>
-                              <input  class="form-control" name="answer" id="answer" required></input><br>
-
-                              <button type="submit" class="btn btn-primary col start" name="submit_project">
-                                  <i class="fa fa-upload"></i>
-                                  <span>Submit</span>
-                              </button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
+                    <button type="submit" class="btn btn-primary">
+                        <i class="fa fa-upload"></i>
+                        <span>Submit</span>
+                    </button>
+                </form>
             </div>
         </div>
-    </section>
+    </div>
 </div>
-
-@endsection
-
--- resources/views/faqs/index.blade.php
+++ resources/views/faqs/index.blade.php
@@ -1,53 +1,58 @@
-@extends('layouts.template') @section('content') <div class="content-wrapper">
+@extends('layouts.template')
+
+@section('content')
+<div class="content-wrapper">
     <section class="content-header">
     </section>
     <section class="content">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-12">
+                <div class="col-md-12">
                     <div class="card">
                         <div class="card-body">
-                            <h3 class="card-title my-1"><i class="fa fa-book"></i> <b>Submitted Projects</b></h3> <br><br>
-                            <h2>FAQs</h2>
-                            <ul class="nav navbar-right panel_toolbox">
-                                <a href="{{ route('create.faq') }}" class="btn btn-round btn-success">
-                                    <i class="fa fa-plus-square"></i> Add FAQ </a>
-                            </ul>
-                            <div class="clearfix"></div>
-                        </div>
-                        <div class="x_content">
-                            <div class="col-md-6 col-sm-6  ">
-                                <div class="x_panel">
-                                    <div class="x_title">
-                                        <h2>Frequently Ask Questions</h2>
-                                        <div class="clearfix"></div>
-                                    </div>
-                                    <div class="x_content">
-                                        <ul class="list-unstyled timeline"> @foreach($faqs as $faq) <li>
-                                                <div class="block">
-                                                    <div class="tags">
-                                                        <a href="" class="tag">
-                                                            <span>Entertainment</span>
-                                                        </a>
-                                                    </div>
-                                                    <div class="block_content">
-                                                        <h2 class="title">
-                                                            <a>{{ $faq->question }}</a>
-                                                        </h2>
-                                                        <div class="byline">
-                                                            <span>13 hours ago</span> by <a>Jane Smith</a>
-                                                        </div>
-                                                        <p class="excerpt">{{ $faq->answer }}</a></p>
-                                                        <a href="{{ route('update.faq', $faq->id) }}" type="button" class="btn btn-sm btn-warning">Edit </a>
-                                                        <button class="btn btn-sm btn-danger" onclick="confirmDelete('{{ route('destroy.faq', $faq->id) }}')"> Delete </button>
-                                                    </div>
+                            <h1 class="mt-5 mb-4 text-center">Frequently Asked Questions</h1>
+                            <div class="tab-content">
+                                <div class="tab-pane active" id="timeline">
+                                    @foreach($faqs as $faq)
+                                    <div class="timeline timeline-inverse">
+                                        <div class="time-label">
+                                            <span class="bg-info">{{ $faq->created_at->format('j M. Y') }}</span>
+                                        </div>
+                                        <div>
+                                            <i class="fas fa-question bg-warning"></i>
+                                            <div class="timeline-item">
+                                                <span class="time"><i class="far fa-clock"></i> {{ \Carbon\Carbon::parse($faq->created_at)->format('h:i A') }}</span>
+                                                <h3 class="timeline-header"><a href="#">{{ $faq->question }}</a></h3>
+                                                <div class="timeline-body">
+                                                    {!! $faq->answer !!}
                                                 </div>
-                                            </li> @endforeach </ul>
+                                                <div class="timeline-footer">
+                                                    <a href="#" class="btn btn-secondary btn-sm">Read more</a>
+                                                    <a href="{{ route('update.faq', $faq->id) }}" type="button" class="btn btn-sm btn-warning">Edit </a>
+                                                    <button class="btn btn-sm btn-danger" onclick="confirmDelete('{{ route('destroy.faq', $faq->id) }}')"> Delete </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    @endforeach
+                                    <!-- Floating button -->
+                                    <div class="float-button">
+                                        <button  type="button"  class="btn btn-primary rounded-circle custom-btn" onclick="location.href='{{ route('create.faq') }}'" data-toggle="modal" data-backdrop="static" data-keyboard="false" data-target="#ictramCreateProblemModal">
+                                            <i class="fa fa-plus"></i>
+                                        </button>
+                                        @include('faqs.create')
+
+                                
+
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                  </section>
-                </div> 
-                @endsection
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+@endsection
+
