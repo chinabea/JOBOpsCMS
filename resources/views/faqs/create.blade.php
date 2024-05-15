984fb145a5eb9bd04b16166b401e5750b565160b
China T. Bea <chibea@my.cspc.edu.ph>
2024-05-16 05:53:15 +0800

    refactor: updated create faq

--- resources/views/faqs/create.blade.php
+++ resources/views/faqs/create.blade.php
@@ -1,6 +1,6 @@
 
 <div class="modal fade" id="createFaqModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
                 <h4 class="modal-title" id="myModalLabel">Add FAQ</h4>
@@ -17,10 +17,10 @@
                     <label for="answer">Answer</label><br>
                     <textarea id="summernote" class="summernote" name="answer" required></textarea><br>
 
-                    <button type="submit" class="btn btn-primary">
-                        <i class="fa fa-upload"></i>
-                        <span>Submit</span>
-                    </button>
+                    <div class="modal-footer justify-content-between">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Add FAQ</button>
+                    </div>
                 </form>
             </div>
         </div>
