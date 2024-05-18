1670450bf3599833456baa6b88b7745d5a6171b1
China T. Bea <chibea@my.cspc.edu.ph>
2024-05-18 19:27:42 +0800

    feat: updated role user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,6 +29,7 @@ class DatabaseSeeder extends Seeder
             [
                 'name' => 'China Bea',
                 'email' => 'chibea@my.cspc.edu.ph ',
+                // Director
                 'role' => 1,
                 'avatar' => 'https://lh3.googleusercontent.com/a-/ALV-UjUyzMg5jnHLaIdqVOMbOfKy5sg3jjS-0QBMjHXk14sVzI10lSZcGBwjtKQj1k0fSphccVgWSuhjQiJZKUauEH1erHlTV1egHnFzXInY_x44JRpyRIcoCTkHwbVLAzUzapUs-_QDlfwPuK9yFLAalRQkmTRGSbe1YPxixEU5pvK8wbgRc92cNgV727ERWMi33Wglb_7ZlF78xYQ2dcui4XMpmU5-2mH9NmeZ5blLPgovGEyoUQfFHMsM9cj6OIy7030e5RRyKkfOoqB6-ogVj9wI_9C8oZJarYQYUurz_P7gQokUhVMI0QtnkeUaKbzrFsltN8V-ukM7_2Ji75-T_H2z5RVTGn9KGYzWxIRso9yxuhTXVjkYNDRndVCP8SapkCIynkdD-LHcmiaWsUGOb8Cn35FKlXAkFV00l_J8EME4LB2vGdENNCP2Bje6P0ijO9JnKgpr7HiON4w9rK5b8hsLqmjXqdA9y7EsBrzhGQvIoTB8IwsN0mZPQM5fg33WeaSebgAoqejm-iNTYVV8_i171jSm0tdu5e-5uMlmguJKFVMOOsqtTmuqvImSdVRGnEaXrluab2M4FX2Q2b0TburziOpuDhTm6fIbjz3J3aMSkAofR4fzr8NZphKDW30xNO_mzy50o2bC0tYopiD_tL1rj-IRfqIO-xY_TwrsPt3ngC3T6599U5_896dPFrffAlEtogH7t9HaQdHxKgH3NiPhJC0DWj5LBdUehJGcF9sMRFz6W2LnO6oXAneItDFQYL1TiBs8uFcChd4PFKRakFDUyLwgG8Lq_ewVh8kAu-hkZ_qamKYO5DdZWfMTCZYacDDZWP6sY9pOR_C_Yw94Kn06KfgWcxn-lAAFueSO5JVZG20o4bl9hrxg46Eq9vg5QAhFTZ1SENMKkLhplgBNHS3V_ruGNDdZkHtvZdXr9VCAl6J2io_H9dvkOOs9LN7P8sMiioZ8u4uKBPYOLhG0BNam4Q=s96-c',
                 'is_approved' => true,
@@ -40,6 +41,7 @@ class DatabaseSeeder extends Seeder
             [
                 'name' => 'Mark Louis Odavar',
                 'email' => 'mlodavar@my.cspc.edu.ph',
+                // ICTRAM Head
                 'role' => 2,
                 'avatar' => 'https://lh3.googleusercontent.com/a/ACg8ocIGM3-G1PBRp1kRe61ZRM4xR67hq-bUW4O3BkjhHdyDisp6bOM=s96-c',
                 'is_approved' => true,
@@ -50,7 +52,8 @@ class DatabaseSeeder extends Seeder
             [
                 'name' => 'Jessemri Tabayag',
                 'email' => 'jestabayag@my.cspc.edu.ph',
-                'role' => 2,
+                // NICMU Head
+                'role' => 3,
                 'avatar' => 'https://lh3.googleusercontent.com/a/ACg8ocKjGmqvq6Rrri0tkpCM3vx2q7lZc30Wm2OUs1BFdeq-_-8jLF-n=s96-c',
                 'is_approved' => true,
                 'phone_number' => '09248769368',
@@ -60,7 +63,8 @@ class DatabaseSeeder extends Seeder
             [
                 'name' => 'John Carlo Dacara',
                 'email' => 'johdacara@my.cspc.edu.ph',
-                'role' => 2,
+                // MIS Head
+                'role' => 4,
                 'avatar' => 'https://lh3.googleusercontent.com/a/ACg8ocKSDUrjoiSNypUvGZdT8iNqHHnd7A5ATUycaAP6GbH6pH9GDg=s96-c',
                 'is_approved' => true,
                 'phone_number' => '09248769368',
@@ -71,7 +75,8 @@ class DatabaseSeeder extends Seeder
             [
                 'name' => 'JESSICA MATAYA',
                 'email' => 'jesmataya@my.cspc.edu.ph',
-                'role' => 3,
+                // MICT Staff
+                'role' => 5,
                 'avatar' => 'https://lh3.googleusercontent.com/a/ACg8ocKUB0nARYGxuJCevZG8KLnVwpmTDrJsZfFZa7ZdFCLaiJ49pWfI=s96-c',
                 'is_approved' => true,
                 'phone_number' => '09386390756',
@@ -81,6 +86,7 @@ class DatabaseSeeder extends Seeder
             [
                 'name' => 'Louie Molina',
                 'email' => 'loumolina@my.cspc.edu.ph',
+                // Staff
                 'role' => 4,
                 'avatar' => 'https://lh3.googleusercontent.com/a/ACg8ocLIEi3iP9WuXqHV_W1U__h7MFcAFPmwnDCOSmJjhHErFqxQ2g=s96-c',
                 'is_approved' => false,
@@ -92,7 +98,8 @@ class DatabaseSeeder extends Seeder
             [
                 'name' => 'Rica Theresa Adante',
                 'email' => 'ricadante@my.cspc.edu.ph',
-                'role' => null,
+                // Student
+                'role' => 5,
                 'avatar' => 'https://lh3.googleusercontent.com/a/ACg8ocLDuPOe6NaSoJcu5ATbEJsn3ptMJlkAgp3awFZQCY8cqCl4bA=s96-c',
                 'is_approved' => false,
                 'phone_number' => null,
