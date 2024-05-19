24c04c258370fc9f0322b7226215bc82840ad892
Mark Louis R. Odavar <122847155+Cintoshy@users.noreply.github.com>
2024-05-19 14:43:16 +0800

    feat(ictrams index): finished UI and functionalities

---
+++ resources/views/units/ictram/modal/create-jobType.blade.php
@@ -0,0 +1,28 @@
+
+
+<div class="modal fade" id="ictramCreateJobTypeModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" id="myModalLabel">Add Job Type</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form id="jobForm" action="{{ route('ictrams.storeJobType') }}" method="POST">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="jobType">Job Type</label>
+                            <input type="text" class="form-control" id="jobType_name" name="jobType_name" placeholder="Enter job type" required>
+                    </div>
+
+                    <div class="modal-footer justify-content-between">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Add</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
