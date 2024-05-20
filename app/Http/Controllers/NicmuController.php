1eeea621214011fe8fee7bce3fce736139abe383
Mark Louis R. Odavar <122847155+Cintoshy@users.noreply.github.com>
2024-05-20 12:24:00 +0800

    refactor: change the function of storing relationship

--- app/Http/Controllers/NicmuController.php
+++ app/Http/Controllers/NicmuController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\NicmuJobType;
 use App\Models\NicmuEquipment;
 use App\Models\NicmuProblem;
+use Illuminate\Support\Facades\Validator;
 
 class NicmuController extends Controller
 {
@@ -31,22 +32,59 @@ class NicmuController extends Controller
                 'nicmus_job_type_id' => 'unique:nicmus,nicmus_job_type_id,NULL,id,nicmus_equipment_id,' . $request->input('nicmus_equipment_id') . ',ictram_problem_id,' . $request->input('ictram_problem_id'),
             ]);
 
-            if ($validator->fails()) {
-                return redirect()->back()
-                            ->withErrors($validator)
-                            ->withInput()
-                            ->with('success', 'The data you choose is existing.');
-            }
+            // if ($validator->fails()) {
+            //     return redirect()->back()
+            //                 ->withErrors($validator)
+            //                 ->withInput()
+            //                 ->with('success', 'The data you choose is existing.');
+            // }
 
+        $jobTypeId = $request->input('nicmu_job_type_id');
+        $equipmentId = $request->input('nicmu_equipment_id');
+        $problemIds = $request->input('nicmu_problem_ids');
+
+        foreach ($problemIds as $problemId) {
             Nicmu::create([
-                'nicmu_job_type_id' => $request->input('nicmu_job_type_id'),
-                'nicmu_equipment_id' => $request->input('nicmu_equipment_id'),
-                'nicmu_problem_id' => $request->input('nicmu_problem_id'),
+                'nicmu_job_type_id' => $jobTypeId,
+                'nicmu_equipment_id' => $equipmentId,
+                'nicmu_problem_id' => $problemId,
             ]);
+        }
         
 
         return redirect()->route('nicmus.index')->with('success', 'NICMU Saved successfully.');
     }
+    //     public function storeWithRelationShip(Request $request)
+    // {   
+    //     $validator = Validator::make($request->all(), [
+    //         'ictram_job_type_id' => 'required|exists:ictram_job_types,id',
+    //         'ictram_equipment_id' => 'required|exists:ictram_equipments,id',
+    //         'ictram_problem_ids' => 'required|array',
+    //         'ictram_problem_ids.*' => 'exists:ictram_problems,id',
+    //         'ictram_job_type_id' => 'unique:ictrams,ictram_job_type_id,NULL,id,ictram_equipment_id,' . $request->input('ictram_equipment_id'),
+    //     ]);
+
+    //     // if ($validator->fails()) {
+    //     //     return redirect()->back()
+    //     //                 ->withErrors($validator)
+    //     //                 ->withInput()
+    //     //                 ->with('success', 'The data you choose is existing.');
+    //     // }
+
+    //     $jobTypeId = $request->input('ictram_job_type_id');
+    //     $equipmentId = $request->input('ictram_equipment_id');
+    //     $problemIds = $request->input('ictram_problem_ids');
+
+    //     foreach ($problemIds as $problemId) {
+    //         Ictram::create([
+    //             'ictram_job_type_id' => $jobTypeId,
+    //             'ictram_equipment_id' => $equipmentId,
+    //             'ictram_problem_id' => $problemId,
+    //         ]);
+    //     }
+
+    //     return redirect()->route('ictrams.index')->with('success', 'ICTRAM Saved successfully.');
+    // }
 
         public function storeJobType(Request $request)
     {
