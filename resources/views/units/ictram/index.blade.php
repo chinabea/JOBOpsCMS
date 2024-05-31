9d9c5de19a94ca37fadce0377ebf9490975717ae
Mark Louis R. Odavar <122847155+Cintoshy@users.noreply.github.com>
2024-05-31 13:23:11 +0800

    Update index.blade.php

--- resources/views/units/ictram/index.blade.php
+++ resources/views/units/ictram/index.blade.php
@@ -14,68 +14,66 @@
         </div>
       </div>
 
+
     </section>
     <section class="content">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12">
                     <div class="card px-3 pt-3 pb-1">
-                        @include('units.ictram.modal.create-jobType')
-                        @include('units.ictram.modal.create-equipment')
-                        @include('units.ictram.modal.create-problem')
                         <form id="jobForm" action="{{ route('ictrams.add-relation') }}" method="POST">
-                                @csrf
+                            @csrf
                             <div class="d-md-flex flex-md-row flex-column justify-content-between gap-3">
                             <div class="mx-2 w-100">
-                                <div class="d-flex flex-row justify-content-between align-items-center mb-1 mt-2">
+                                <div class="d-flex align-items-center mb-1 mt-2">
                                     <label for="jobType">Job Type</label>
-                                    <div>
-                                    <button type="button" class="btn btn-outline-primary float-right" data-toggle="modal" data-backdrop="static" data-keyboard="false" data-target="#ictramCreateJobTypeModal">
-                                        <i class="fas fa-plus"></i>
-                                    </button>
-                                    </div>
                                 </div>
                                 <div class="dropdown">
-                                    <select name="ictram_job_type_id" id="ictram_job_type_id" class="selectpicker form-control" data-live-search="true" required>
+                                    <select name="ictram_job_type_id" id="ictram_job_type_id" class="selectpicker form-control" data-live-search="true" onchange="showfield(this.value, 'div1')"required>
                                     <option value="" disabled selected>Select Job Type</option>
                                         @foreach($jobTypes as $jobType)
                                             <option value="{{ $jobType->id }}">{{ $jobType->jobType_name }}</option>
                                         @endforeach
+                                        <div class="fixed"><option value="jobType">Other Sources</option></div>
                                     </select>
+                                    <div class="form-group">
+                                        <div class="w-100" id="div1"></div>
+                                    </div>
                                 </div>
                             </div>
                             
                             <div class="mx-2 w-100">
-                                <div class="d-flex flex-row justify-content-between align-items-center mb-1 mt-2">
+                                <div class="d-flex align-items-center mb-1 mt-2">
                                     <label for="equipment">Equipment</label>
-                                    <button type="button" class="btn btn-outline-primary float-right" data-toggle="modal" data-backdrop="static" data-keyboard="false" data-target="#ictramCreateEquipmentModal">
-                                        <i class="fas fa-plus"></i>
-                                    </button>
                                 </div>
                                 <div class="dropdown">
-                                    <select name="ictram_equipment_id" id="ictram_equipment_id" class="selectpicker form-control" data-live-search="true" required>
+                                    <select name="ictram_equipment_id" id="ictram_equipment_id" class="selectpicker form-control" data-live-search="true" onchange="showfield(this.value, 'div2')" required>
                                         <option value="" disabled selected>Select Equipment</option>
                                         @foreach($equipments as $equipment)
                                             <option value="{{ $equipment->id }}">{{ $equipment->equipment_name }}</option>
                                         @endforeach
+                                        <div class="fixed"><option value="equipment">Other Sources</option></div>
+                                    </select>
+                                    <div class="form-group">
+                                        <div class="w-100" id="div2"></div>
+                                    </div>
                                     </select>
                                 </div>
                             </div>
                             <div class="mx-2 w-100">
-                                <div class="d-flex flex-row justify-content-between align-items-center mb-1 mt-2">
+                                <div class="d-flex align-items-center mb-1 mt-2">
                                     <label for="jobType">Problem</label>
-                                    <div>
-                                    <button type="button" class="btn btn-outline-primary float-right" data-toggle="modal" data-backdrop="static" data-keyboard="false" data-target="#ictramCreateProblemModal">
-                                        <i class="fas fa-plus"></i>
-                                    </button>
-                                    </div>
                                 </div>
                                 <div class="dropdown">
-                                    <select name="ictram_problem_ids[]" id="ictram_problem_id" class="selectpicker form-control" data-live-search="true" multiple required>
+                                    <select name="ictram_problem_ids[]" id="ictram_problem_id" class="selectpicker form-control" data-live-search="true" onchange="showfield(this.value, 'div3')" multiple>
                                         @foreach ($problems as $problem)
                                         <option value="{{ $problem->id }}">{{ $problem->problem_description }}</option>
                                         @endforeach
+                                        <div class="fixed"><option value="problem">Other Sources</option></div>
                                     </select>
+                                    <div class="form-group">
+                                        <div class="w-100" id="div3"></div>
+                                    </div>
                                 </div>
                             </div>
                             </div>
@@ -124,3 +122,25 @@
         overflow-y: auto;
     }
 </style>
+<script type="text/javascript">
+function showfield(name, divId){
+    var divElement = document.getElementById(divId);
+    var selectElement = document.getElementById('ictram_problem_id');
+    var selectedValues = Array.from(selectElement.selectedOptions).map(option => option.value);
+
+    if (name === 'jobType') {
+        divElement.innerHTML = 'Other: <input class="form-control" type="text" name="jobType_other" placeholder="Please Specify JobType" required/>';
+    } else if (name === 'equipment') {
+        divElement.innerHTML = 'Other: <input class="form-control" type="text" name="equipment_other" placeholder="Please Specify Equipment" required/>';
+    } else if (selectedValues.includes('problem')) {
+        selectElement.value = 'problem';
+        divElement.innerHTML = 'Other: <input class="form-control" type="text" name="problem_other" placeholder="Please Specify Problem" required/>';
+        // divElement.removeAttribute("multiple");
+        divElement.click();
+    } else {
+        divElement.innerHTML = '';
+    }
+}
+</script>
+
+
