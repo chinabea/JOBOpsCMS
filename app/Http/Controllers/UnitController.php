ed6308206793efece4863597761bf6b210dd984d
China T. Bea <chibea@my.cspc.edu.ph>
2024-05-31 15:06:04 +0800

    update unit controller

--- app/Http/Controllers/UnitController.php
+++ app/Http/Controllers/UnitController.php
@@ -49,18 +49,6 @@ class UnitController extends Controller
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
     public function purchased()
     {
         // Pass the data to the view
@@ -72,7 +60,7 @@ class UnitController extends Controller
         try {
             // Retrieve all tickets with their associated user (who created the ticket) and the assigned users.
             // Including 'user' in the with clause assumes you have a separate relationship defined in Ticket model to fetch the creator of the ticket
-            $tickets = Ticket::with(['user', 'users'])->orderBy('created_at', 'desc')->get();
+            $tickets = Ticket::orderBy('created_at', 'desc')->get();
             $userIds = User::where('role', 2)->where('is_approved', true)->get();
             
             return view('ticket.ictram-tickets', compact('tickets','userIds'));
@@ -85,7 +73,7 @@ class UnitController extends Controller
         try {
             // Retrieve all tickets with their associated user (who created the ticket) and the assigned users.
             // Including 'user' in the with clause assumes you have a separate relationship defined in Ticket model to fetch the creator of the ticket
-            $tickets = Ticket::with(['user', 'users'])->orderBy('created_at', 'desc')->get();
+            $tickets = Ticket::orderBy('created_at', 'desc')->get();
             $userIds = User::where('role', 2)->where('is_approved', true)->get();
             
             return view('ticket.nicmu-tickets', compact('tickets','userIds'));
@@ -98,7 +86,7 @@ class UnitController extends Controller
         try {
             // Retrieve all tickets with their associated user (who created the ticket) and the assigned users.
             // Including 'user' in the with clause assumes you have a separate relationship defined in Ticket model to fetch the creator of the ticket
-            $tickets = Ticket::with(['user', 'users'])->orderBy('created_at', 'desc')->get();
+            $tickets = Ticket::orderBy('created_at', 'desc')->get();
             $userIds = User::where('role', 2)->where('is_approved', true)->get();
             
             return view('ticket.mis-tickets', compact('tickets','userIds'));
