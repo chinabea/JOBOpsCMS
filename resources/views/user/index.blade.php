3464f754f4b6000aa8f360bb97abf8d149916d2c
China T. Bea <chibea@my.cspc.edu.ph>
2024-06-08 06:06:17 +0800

    rearrange users

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,80 +1,82 @@
 
+@extends('layouts.template')
 
-@extends('layouts.template') @section('content') <div class="content-wrapper">
+@section('content')
+<div class="content-wrapper">
     <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Users</h1>
+                </div>
+                <div class="col-sm-6">
+                    <div class="d-flex justify-content-end">
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
+
     <section class="content">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12">
                     <div class="card">
                         <div class="card-body">
-                            <h3 class="card-title my-1"><i class="fa fa-book"></i> <b>Submitted Projects</b></h3> <br><br>
-                            <form action="{{ route('generate.users.report') }}" method="post">
-                                @csrf
-                                <div class="row justify-content-center">
-                                    <div class="form-group row">
-                                        <div class="col-md-6">
-                                            <label for="start_date">Start Date:</label>
-                                            <input type="date" class="form-control" name="start_date" id="start_date">
-                                        </div>
-                                        <div class="col-md-6">
-                                            <label for="end_date">End Date:</label>
-                                            <input type="date" class="form-control" name="end_date" id="end_date">
-                                        </div>
-                                    </div>
-                                <div class="col-lg-4">
-                                    <div class="form-group">
-                                        <label>Actions</label>
-                                        <div>
-                                            <button type="button" id="reset" class="btn btn-warning"><i class="fa fa-refresh"></i> </button>
-                                            <button type="submit" class="btn btn-info"><i class="fa fa-file-pdf"></i> Generate PDF</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </form>
-
+                            <p class="mb-4"></p>
                             <table id="example1" class="table table-bordered table-hover text-center table-striped table-sm">
                                 <thead>
                                     <tr>
                                         <th>#</th>
-                                        <th><i class="fa fa-info-circle"></i> Name</th>
-                                        <th><i class="fa fa-envelope"></i> Email</th>
-                                        <th><i class="fa fa-sitemap"></i> Role</th>
-                                        <th><i class="fa fa-sitemap"></i> Approval</th>
-                                        <th><i class="fa fa-pencil-square-o"></i> Action(s)</th>
+                                        <th>Name</th>
+                                        <th>Email</th>
+                                        <th>Role</th>
+                                        <th>Approval</th>
+                                        <th>Action(s)</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     @foreach($users as $user)
                                     <tr>
-                                        <td class="align-middle">{{ $loop->iteration }}</td>
-                                        <td class="align-middle">{{ $user->name }}</td>
-                                        <td class="align-middle">{{ $user->email }}</td>
-                                        <td class="align-middle">
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $user->name }}</td>
+                                        <td>{{ $user->email }}</td>
+                                        <td>
                                             @if ($user->role == 1)
-                                                Admin
+                                                Director
                                             @elseif ($user->role == 2)
-                                                MICT Staff
+                                                ICTRAM Head
                                             @elseif ($user->role == 3)
+                                                NICMU Head
+                                            @elseif ($user->role == 4)
+                                                MIS Head
+                                            @elseif ($user->role == 5)
                                                 Staff
+                                            @elseif ($user->role == 6)
+                                                Student
+                                            @elseif ($user->role == 7)
+                                                ICTRAM Staff
+                                            @elseif ($user->role == 8)
+                                                NICMU Staff
+                                            @elseif ($user->role == 9)
+                                                MIS Staff
                                             @else
                                                 Guest
                                             @endif
                                         </td>
                                         <td>
                                             @if (!$user->is_approved)
-                                                <a href="{{ route('users.approve', $user->id) }}" class="btn btn-sm btn-info"><i class="fa fa-check-square-o"></i> Approve</a>
+                                                <a href="{{ route('users.approve', $user->id) }}" class="btn btn-sm btn-info"><i class="fa fa-check-square"></i> Approve User</a>
                                             @else
-                                                <a href="{{ route('users.disapprove', $user->id) }}" class="btn btn-sm btn-danger"><i class="fa fa-times-circle"></i> Disapprove</a>
+                                                <a href="{{ route('users.disapprove', $user->id) }}" class="btn btn-sm btn-warning"><i class="fa fa-times-circle"></i> Disapprove User</a>
                                             @endif
                                         </td>
-                                        <td class="align-middle">
+                                        <td>
                                             <a href="{{ route('user.edit', $user->id) }}" class="btn btn-sm btn-primary">
-                                                <i class="fa fa-user"></i> View Profile
+                                                <i class="fa fa-user"></i> View User Profile
                                             </a>
                                             <button class="btn btn-danger btn-sm" onclick="confirmDelete('{{ route('user.destroy', $user->id) }}')">
-                                                <i class="fa fa-trash"></i>
+                                                <i class="fa fa-trash"></i> Delete
                                             </button>
                                         </td>
                                     </tr>
@@ -87,4 +89,5 @@
             </div>
         </div>
     </section>
-</div> @endsection
+</div>
+@endsection
--- resources/views/user/setup-profile.blade.php
+++ resources/views/user/setup-profile.blade.php
@@ -7,12 +7,16 @@
   <div class="container">
     <div class="row no-gutters justify-content-center mt-5">
 
-      <div class="col-lg-8 box featured" data-aos="fade-up">
-        <h4>Setup your Profile</h4>
-        <ul>
+    <section id="contact" class="contact">
+      <div class="container aos-init aos-animate" data-aos="fade-up">
+        <header class="section-header">
+          <p>Contact Us</p>
+        </header>
+        <div class="row gy-4">
           <form action="{{ route('user.saveProfile') }}" method="POST">
             @csrf
-              <div class="form-group mt-3">
+            
+            <div class="form-group mt-3">
                 <input class="form-control" type="text" id="name" name="name" value="{{ $user->name }}" disabled>
             </div>
               <div class="form-group mt-3">
@@ -24,17 +28,22 @@
             <div class="form-group mt-3">
               <select class="form-control" name="role" required>
                 <option value="" selected disabled>Select your Role</option>
-                <option value="1" type="Admin" id="1" name="1">Admin</option>
-                <option value="2" type="MICT Staff" id="2" name="2">MICT Staff</option>
-                <option value="3" type="Staff" id="3" name="3">Staff</option>
+                <option value="1" type="Director" id="1" name="1">Director</option>
+                <option value="2" type="ICTRAM Head" id="2" name="2">ICTRAM Head</option>
+                <option value="3" type="NICMU Head" id="3" name="3">NICMU Head</option>
+                <option value="4" type="MIS Head" id="4" name="4">MIS Head</option>
+                <option value="5" type="Staff" id="5" name="5">Staff</option>
+                <option value="6" type="Student" id="6" name="6">Student</option>
+                <option value="7" type="ICTRAM Staff" id="7" name="7">ICTRAM Staff</option>
+                <option value="8" type="NICMU Staff" id="8" name="8">NICMU Staff</option>
+                <option value="9" type="MIS Staff" id="9" name="9">MIS Staff</option>
               </select>
             </div>
-            <div class="form-group mt-3">
+            <!-- <div class="form-group mt-3">
                 <input placeholder="Job Position" class="form-control" type="text" id="job_position" name="job_position" required>
             </div>
             <div class="form-group mt-3" id="expertise-area">
               <div id="dynamic-expertise">
-                  <!-- New expertise entry field -->
                   <div class="input-group mb-2">
                       <input type="text" class="form-control" name="expertise[]" placeholder="Enter expertise">
                       <div class="input-group-append">
@@ -42,27 +51,18 @@
                       </div>
                 </div>
               </div>
-            </div>
-
-            
+            </div> -->
             <br><br>
             <div class="text-center">
-              <button type="submit" class="get-started-btn">Save Profile and Request</button>
+              <button type="submit" class="btn btn-primary">Save Profile and Request</button>
             </div>
-
-        </form>
-        </ul>
+          </form>
+        </div>
       </div>
-      
+    </section>
     </div>
-
   </div>
 </section>
-<!-- End Pricing Section -->
-
-
-
-        
 
 <script>
 function addExpertise() {
