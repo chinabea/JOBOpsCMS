6cbcc1f4c55ead6e3d054e46dff3374fd0892b39
China T. Bea <chibea@my.cspc.edu.ph>
2024-06-08 06:06:34 +0800

    added welcome message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,8 +8,16 @@
   <div class="container">
     <div class="row">
       <div class="col-lg-6 d-flex flex-column justify-content-center mt-5">
+        <!-- Success Message -->
+        @if(session('success'))
+          <div class="alert alert-success" role="alert" data-aos="fade-up">
+            {{ session('success') }}
+          </div>
+        @endif
+        <!-- End Success Message -->
+        
         <h1 data-aos="fade-up">Job Operations Central Management System</h1>
-        <h2 data-aos="fade-up" data-aos-delay="400">We are team of talented designers making websites with Bootstrap</h2>
+        <h2 data-aos="fade-up" data-aos-delay="400">We are a team of talented designers making websites with Bootstrap</h2>
         <div data-aos="fade-up" data-aos-delay="600">
           <div class="text-center text-lg-start">
             <a href="{{ url('login/google') }}" class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
