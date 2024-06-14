27e138d9c71859a7d71cbd324d72a6eda70f0a90
China T. Bea <chibea@my.cspc.edu.ph>
2024-06-14 20:28:40 +0800

    refactor: manage

--- resources/views/manage.blade.php
+++ resources/views/manage.blade.php
@@ -144,24 +144,21 @@
                     <h2 class="mb-2 mt-4">Datas</h2>
                     <div class="row">
                         <div class="col-lg-3 col-6">
-                            <!-- small card -->
                             <div class="small-box bg-info">
-                            <div class="inner">
-                                <h3>{{ $OfficeName }}</h3>
-                                
-                                <p>Office Names</p>
-                            </div>
-                            <div class="icon">
-                                <i class="fas fa-shopping-cart"></i>
-                            </div>
-                            <a href="{{ route('office-names.index') }}" class="small-box-footer">
-                                More info <i class="fas fa-arrow-circle-right"></i>
-                            </a>
+                                <div class="inner">
+                                    <h3>{{ $OfficeName }}</h3>
+                                    
+                                    <p>Office Names</p>
+                                </div>
+                                <div class="icon">
+                                    <i class="fas fa-shopping-cart"></i>
+                                </div>
+                                <a href="{{ route('office-names.index') }}" class="small-box-footer">
+                                    More info <i class="fas fa-arrow-circle-right"></i>
+                                </a>
                             </div>
                         </div>
-                        <!-- ./col -->
                         <div class="col-lg-3 col-6">
-                            <!-- small card -->
                             <div class="small-box bg-success">
                             <div class="inner">
                                 <h3>{{ $buildingNumber }}</h3>
@@ -176,49 +173,8 @@
                             </a>
                             </div>
                         </div>
-                        <!-- ./col -->
-                        <div class="col-lg-3 col-6">
-                            <!-- small card -->
-                            <div class="small-box bg-warning">
-                            <div class="inner">
-                                <h3>44</h3>
-
-                                <p>User Registrations</p>
-                            </div>
-                            <div class="icon">
-                                <i class="fas fa-user-plus"></i>
-                            </div>
-                            <a href="#" class="small-box-footer">
-                                More info <i class="fas fa-arrow-circle-right"></i>
-                            </a>
-                            </div>
-                        </div>
-                        <!-- ./col -->
-                        <div class="col-lg-3 col-6">
-                            <!-- small card -->
-                            <div class="small-box bg-danger">
-                            <div class="inner">
-                                <h3>65</h3>
-
-                                <p>Unique Visitors</p>
-                            </div>
-                            <div class="icon">
-                                <i class="fas fa-chart-pie"></i>
-                            </div>
-                            <a href="#" class="small-box-footer">
-                                More info <i class="fas fa-arrow-circle-right"></i>
-                            </a>
-                            </div>
-                        </div>
-                        <!-- ./col -->
-                        </div>
-
-
-
-
-
-
-
+                    </div>
+                    
                 </div>
             </div>
         </div>
