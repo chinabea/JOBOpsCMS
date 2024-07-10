6f85698f3f239f2775d936b256ec0233d147f4e1
China T. Bea <chibea@my.cspc.edu.ph>
2024-07-10 21:04:43 +0800

    remove commented code

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -104,56 +104,6 @@ class UserController extends Controller
         }
     }
 
-    
-    // public function edit($id)
-    // {
-    //     try {
-    //         // Retrieve and show the specific item for editing
-    //         $user = User::findOrFail($id);
-        
-    //         // Here, we define the roles. In a real application, consider retrieving these from a config or database.
-    //         $roles = [1 => 'Admin', 2 => 'MICT Staff', 3 => 'Staff']; 
-    
-    //         // Define the expertise options
-    //         $expertiseOptions = ['Web Development', 'Graphic Design', 'Data Analysis', 'Project Management'];
-            
-    //         // Convert the comma-separated string of expertise to an array
-    //         // $user->expertise = !empty($user->expertise) ? explode(',', $user->expertise) : [];
-            
-    // // Convert the comma-separated string of expertise to an array if not empty
-    // $expertiseArray = !empty($user->expertise) ? explode(',', $user->expertise) : [];
-
-                    
-                
-    //         // Fetch tickets where the user is assigned
-    //         $assignedTickets = Ticket::whereHas('users', function ($query) use ($user) {
-    //             $query->where('users.id', $user->id);
-    //         })
-    //         ->with(['user', 'users']) // Load relationships more properly
-    //         ->orderBy('created_at', 'desc')
-    //         ->get();
-            
-        
-    //         // Example of existing expertise assuming it's stored as an array or collection
-    //         $existingExpertise = $user->expertise; // Ensure this is defined in your User model
-
-    //         $monthlyTicketsData = Ticket::select(DB::raw("YEAR(created_at) as year"), DB::raw("MONTH(created_at) as month"), DB::raw("COUNT(*) as count"))
-    //         ->where('user_id', $user->id)
-    //         ->groupBy('year', 'month')
-    //         ->orderBy('year', 'asc')
-    //         ->orderBy('month', 'asc')
-    //         ->get();
-        
-        
-                    
-    //         return view('user.user-profile', compact('user', 'roles', 'expertiseOptions', 'assignedTickets',
-    //          'existingExpertise','monthlyTicketsData'));
-    //     } catch (Exception $e) {
-    //         // Handle the exception here, you can log it or return an error response
-    //         return $e->getMessage();
-    //     }
-    // }
-
     public function update(Request $request, $id)
     {
         try {
