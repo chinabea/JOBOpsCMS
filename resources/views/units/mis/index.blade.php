d10cf1b0c0535766b812328e069857102893e034
China T. Bea <chibea@my.cspc.edu.ph>
2024-07-12 20:35:57 +0800

    update units mis index.blade.php

--- resources/views/units/mis/index.blade.php
+++ resources/views/units/mis/index.blade.php
@@ -9,7 +9,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">MISes Assignation</h1>
+            <h1 class="m-0">MIS Assignation</h1>
           </div>
         </div>
       </div>
