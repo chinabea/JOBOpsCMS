36361fbd9a81b2964386ffda7c227b375f4d725d
China T. Bea <chibea@my.cspc.edu.ph>
2024-07-15 06:42:48 +0800

    update: ticket show.blade.php to xl modal

--- resources/views/ticket/show.blade.php
+++ resources/views/ticket/show.blade.php
@@ -1,7 +1,7 @@
 
 <!-- Modal -->
 <div class="modal fade" id="showTicketModal-{{ $ticket->id }}" tabindex="-1" role="dialog" aria-labelledby="showTicketModalLabel-{{ $ticket->id }}" aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-dialog modal-xl" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="showTicketModalLabel">View Ticket</h5>
@@ -9,134 +9,141 @@
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <div class="modal-body text-left">
-                <!-- Ticket Details -->
-                <h5 class="mt-3">Ticket Details</h5>
-                <div class="form-group">
-                    <label for="requested_by">Requested by</label>
-                    <input type="text" class="form-control" id="user_id" value="{{ $ticket->user->name }}" disabled>
-                </div>
-                <div class="form-group">
-                    <label for="service_location">Building</label>
-                    <input type="text" class="form-control" id="buildingNumber" value="{{ $ticket->buildingNumber->building_number ?? 'N/A' }}" disabled>
-                </div>
-                <div class="form-group">
-                    <label for="service_location">Office</label>
-                    <input type="text" class="form-control" id="buildingNumber" value="{{ $ticket->officeName->office_name ?? 'N/A' }}" disabled>
-                </div>
-                <div class="form-group"> 
-                    <label for="service_location">Unit Request</label>
-                    @if($ticket->ictram)
-                        <input type="text" class="form-control" value="ICTRAM" disabled>
-                    @endif
-                    @if($ticket->nicmu)
-                        <input type="text" class="form-control" value="NICMU" disabled>
-                    @endif
-                    @if($ticket->mis)
-                        <input type="text" class="form-control" value="MIS" disabled>
-                    @endif
-                </div>
-                    <div class="form-group">
-                        <label for="service_location">Job Type</label>
-                        @if($ticket->ictram)
-                        <input type="text" class="form-control" value="{{ $ticket->ictram->jobType->jobType_name }}" disabled>
-                        @endif
-                        @if($ticket->nicmu)
-                        <input type="text" class="form-control" value="{{ $ticket->nicmu->jobType->jobType_name }}" disabled>
-                        @endif
-                        @if($ticket->mis)
-                        <input type="text" class="form-control" value="{{ $ticket->mis->jobType->jobType_name }}" disabled>
-                        @endif
+            <div class="modal-body">
+                <div class="row">
+                    <!-- General Information -->
+                    <div class="col-md-6">
+                        <div class="card card-info">
+                            <div class="card-header">
+                                <h3 class="card-title">Ticket Details</h3>
+                            </div>
+                            <div class="card-body text-left">
+                                <!-- Ticket Details -->
+                                <div class="form-group">
+                                    <label for="user_id">Requested by</label>
+                                    <p class="form-control-static">{{ $ticket->user->name }}</p>
+                                </div>
+                                <div class="form-group">
+                                    <label for="buildingNumber">Building</label>
+                                    <p class="form-control-static">{{ $ticket->buildingNumber->building_number ?? 'N/A' }}</p>
+                                </div>
+                                <div class="form-group">
+                                    <label for="officeName">Office</label>
+                                    <p class="form-control-static">{{ $ticket->officeName->office_name ?? 'N/A' }}</p>
+                                </div>
+                                <div class="form-group">
+                                    <label for="unit_request">Unit Request</label>
+                                    <p class="form-control-static">{{ $ticket->ictram ? 'ICTRAM' : ($ticket->nicmu ? 'NICMU' : ($ticket->mis ? 'MIS' : 'N/A')) }}</p>
+                                </div>
+                                <div class="form-group">
+                                    <label for="job_type">Job Type</label>
+                                    <p class="form-control-static">{{ $ticket->ictram->jobType->jobType_name ?? ($ticket->nicmu->jobType->jobType_name ?? ($ticket->mis->jobType->jobType_name ?? 'N/A')) }}</p>
+                                </div>
+                                <div class="form-group">
+                                    <label for="service_for">Service for</label>
+                                    <p class="form-control-static">{{ $ticket->ictram->equipment->equipment_name ?? ($ticket->nicmu->equipment->equipment_name ?? ($ticket->mis->asName->name ?? 'N/A')) }}</p>
+                                </div>
+                                <div class="form-group">
+                                    <label for="issues_or_concern">Issues or Concern</label>
+                                    <p class="form-control-static">{{ $ticket->ictram->problem->problem_description ?? ($ticket->nicmu->problem->problem_description ?? ($ticket->mis->requestTypeName->requestType_name ?? 'N/A')) }}</p>
+                                </div>
+                                <div class="form-group">
+                                    <label for="assigned_to">Assigned To</label>
+                                    @foreach($ticket->assignedUsers as $user)
+                                        @php
+                                            $escalatedByUser = App\Models\User::find($user->pivot->escalatedBy_for_workloadLimitReached);
+                                        @endphp
+                                        <div class="user-info">
+                                            <div class="form-group">
+                                                <label>Name</label>
+                                                <p class="form-control-static">{{ $user->name }}</p>
+                                            </div>
+                                            <div class="form-group">
+                                                <label>Assigned to Tickets</label>
+                                                <p class="form-control-static">{{ $user->tickets->count() }}</p>
+                                            </div>
+                                            <div class="form-group">
+                                                <label>Escalation Reason for Workload Limit Reached</label>
+                                                <p class="form-control-static">{{ $user->pivot->escalationReason_for_workloadLimitReached ?? 'N/A' }}</p>
+                                            </div>
+                                            <div class="form-group">
+                                                <label>Escalated By</label>
+                                                <p class="form-control-static">{{ $escalatedByUser ? $escalatedByUser->name : 'N/A' }}</p>
+                                            </div>
+                                            <div class="form-group">
+                                                <label>Escalation Reason Due to Client Noncompliance</label>
+                                                <p class="form-control-static">{{ $user->pivot->escalationReasonDue_to_clientNoncompliance ?? 'N/A' }}</p>
+                                            </div>
+                                            <div class="form-group">
+                                                <label>Client Noncompliance File</label>
+                                                <p class="form-control-static">{{ $user->pivot->clientNoncomplianceFile ?? 'N/A' }}</p>
+                                            </div>
+                                        </div>
+                                    @endforeach
+                                </div>
+                                <div class="form-group">
+                                    <label for="priority_level">Priority</label>
+                                    <p class="form-control-static">{{ $ticket->priority_level }}</p>
+                                </div>
+                                <div class="form-group">
+                                    <label for="status">Status</label>
+                                    <p class="form-control-static">{{ $ticket->status }}</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="form-group">
-                        <label for="service_location">Service for</label>
-                        @if($ticket->ictram)
-                        <input type="text" class="form-control" value="{{ $ticket->ictram->equipment->equipment_name }}" disabled>
-                        @endif
-                        @if($ticket->nicmu)
-                        <input type="text" class="form-control" value="{{ $ticket->nicmu->equipment->equipment_name }}" disabled>
-                        @endif
-                        @if($ticket->mis)
-                        <input type="text" class="form-control" value="{{ $ticket->mis->asName->name }}" disabled>
-                        @endif
-                        
-                    </div>
-                    <div class="form-group">
-                        <label for="service_location">Issues or Concern</label>
-                        @if($ticket->ictram)
-                        <input type="text" class="form-control" value="{{ $ticket->ictram->problem->problem_description }}" disabled>
-                        @endif
-                        @if($ticket->nicmu)
-                        <input type="text" class="form-control" value="{{ $ticket->nicmu->problem->problem_description }}" disabled>
-                        @endif
-                        @if($ticket->mis)
-                        <input type="text" class="form-control" value="{{ $ticket->mis->requestTypeName->requestType_name }}" disabled>
-                        @endif
-                    </div>
-                    <div class="form-group">
-                        <label for="priority_level">Priority</label>
-                        <input type="text" class="form-control" id="priority_level" value="{{ $ticket->priority_level }}" disabled>
-                    </div>
-                    <div class="form-group">
-                        <label for="status">Status</label>
-                        <input type="text" class="form-control" id="status" value="{{ $ticket->status }}" disabled>
-                    </div>
-                    <div class="form-group">
-                        <label for="assigned_to">Assigned to</label><br>
-                        @foreach($ticket->assignedUsers as $user)
-                            {{ $user->name }} <br>
-                            Assigned to Tickets: {{ $user->tickets->count() }} - <br>
-                            Escalation Reason for Workload Limit Reached: {{ $user->pivot->escalationReason_for_workloadLimitReached }} - <br>
-                            @php
-                                $escalatedByUser = App\Models\User::find($user->pivot->escalatedBy_for_workloadLimitReached);
-                            @endphp
-                            Escalated By: {{ $escalatedByUser ? $escalatedByUser->name : 'N/A' }} - <br>
-                            Escalation Reason Due to Client Noncompliance: {{ $user->pivot->escalationReasonDue_to_clientNoncompliance }} - <br>
-                            Client Noncompliance File: {{ $user->pivot->clientNoncomplianceFile }}
-                        @endforeach
-                    </div>
+                    <!-- Attachments -->
+                    <div class="col-md-6">
+                        <div class="card card-info">
+                            <div class="card-header">
+                                <h3 class="card-title">Assessment and Action</h3>
+                            </div>
+                            <div class="card-body text-left">
+                                <div class="form-group">
+                                    <label>Initial Assessment</label>
+                                    <p class="form-control-static">{{ $ticket->initial_assessment }}</p>
+                                </div>
+                                <div class="form-group">
+                                    <label for="action_performed">Action Taken</label>
+                                    <p class="form-control-static">{{ $ticket->action_performed }}</p>
+                                </div>
+                                <div class="form-group">
+                                    <label for="file_path">Uploaded File</label>
+                                    <p class="form-control-static">{{ $ticket->file_path }}</p>
+                                </div>
+                            </div>
+                        </div>
 
-                <!-- Assessment and Action -->
-                <h5 class="mt-3">Assessment and Action</h5>
-                <div class="form-group">
-                    <label>Initial Assessment</label>
-                    <p>{{ $ticket->initial_assessment }}</p>
-                </div>
-                <div class="form-group">
-                    <label for="action_performed">Action Taken</label>
-                    <textarea class="form-control" id="action_performed" disabled>{{ $ticket->action_performed }}</textarea>
-                </div>
-                <!-- Upload File -->
-                <div class="form-group">
-                    <label for="file_path">Uploaded File</label>
-                    @if ($ticket->file_path)
-                        <a href="{{ $ticket->file_path }}" target="_blank">View File</a>
-                    @else
-                        <p>No file uploaded</p>
-                    @endif
-                </div>
-                
-                <!-- Additional Information -->
-                <div class="mt-4">
-                    <p><strong>Assigned To:</strong> {{ $ticket->assignedUser ? $ticket->assignedUser->name : 'Unassigned' }}</p>
-                    <p><strong>Escalation Reason:</strong> {{ $ticket->escalation_reason_for_workload_limit_reached ?? 'None' }}</p>
-                </div>
-                
-                <!-- Escalation Information -->
-                @if ($ticket->assigned_user_id == auth()->id() || auth()->user()->role == 1)
-                    <hr>
-                    <h4>Reached the Limit of Workload?</h4>
-                    <p><strong>Escalation Reason:</strong> {{ $ticket->escalation_reason ?? 'None' }}</p>
-                @endif
-                <hr>
-                <h4>Escalation Reason Due to Client Noncompliance</h4>
-                <div class="form-group">
-                    <p><strong>Reason:</strong> {{ $ticket->escalation_reason_due_to_client_noncompliance ?? 'None' }}</p>
-                    @if ($ticket->client_noncompliance_file)
-                        <a href="{{ $ticket->client_noncompliance_file }}" target="_blank">View File</a>
-                    @else
-                        <p>No file uploaded</p>
-                    @endif
+                        <div class="card card-info">
+                            <div class="card-header">
+                                <h3 class="card-title">Reached the Limit of Workload?</h3>
+                            </div>
+                            <div class="card-body text-left">
+                                @if ($ticket->assigned_user_id == auth()->id() || auth()->user()->role == 1)
+                                    <div class="form-group">
+                                        <label for="reason">Escalation Reason:</label>
+                                        <p class="form-control-static">{{ $ticket->reason }}</p>
+                                    </div>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="card card-info">
+                            <div class="card-header">
+                                <h3 class="card-title">Escalation Reason Due to Client Noncompliance</h3>
+                            </div>
+                            <div class="card-body text-left">
+                                <div class="form-group">
+                                    <label for="escalationReasonDue_to_clientNoncompliance">Escalation Reason</label>
+                                    <p class="form-control-static">{{ $ticket->escalationReasonDue_to_clientNoncompliance }}</p>
+                                </div>
+                                <div class="form-group">
+                                    <label for="clientNoncomplianceFile">Client Noncompliance File</label>
+                                    <p class="form-control-static">{{ $ticket->clientNoncomplianceFile }}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
